2d9d85f3cd1309d1f6a23ed13f20e88414ec95b4
Besrour Mohamed <m.besrour@yahoo.com>
2023-09-14 20:32:29 +0200

    debur page produit

---
+++ my-library/src/pages/product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/product.css'
+export default function product() {
+  return (
+    <>
+    <div className='pageProduct'>
+        
+        <div className='containerProduct'></div>
+    </div>
+    </>
+  )
+}
