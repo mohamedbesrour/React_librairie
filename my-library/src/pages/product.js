78032d76a41e4427ed34198b0bcc3c02eb81722b
Besrour Mohamed <m.besrour@yahoo.com>
2023-09-14 22:18:52 +0200

    product avancement 2

--- my-library/src/pages/product.js
+++ my-library/src/pages/product.js
@@ -4,7 +4,7 @@ export default function product() {
   return (
     <>
     <div className='pageProduct'>
-        
+        <h1>La Librairie d'Angélique</h1>
         <div className='containerProduct'></div>
     </div>
     </>
