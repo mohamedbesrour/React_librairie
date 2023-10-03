667cc27179d43b51d80f32a96ff0643eee902b45
mohamedbesrour <118281616+mohamedbesrour@users.noreply.github.com>
2023-10-03 16:03:53 +0200

    db-small-project

---
+++ my-library/client/src/composants/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/footer.css';
+
+
+export default function footer() {
+    return (
+        <>
+        <div className= "FooterContainer">
+         <div className="FooterTitre">
+          <h1>Ma Librairie</h1>
+         </div>
+        </div>
+        </>
+    );
+}
---
+++ my-library/client/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Footer from '../composants/footer'
+import '../styles/home.css'
+export default function Home() {
+  return (
+    <>
+    <div className='pageHome'>
+        <h1>La Librairie d'Angélique</h1>
+        
+    </div>
+  
+    </>
+  )
+}
