061c321be111e13f84243efea8f0a63770ed97ab
Besrour Mohamed <m.besrour@yahoo.com>
2023-11-13 02:51:10 +0100

    db-small-project
    Ma version de db-small-project

--- my-library/backend/index.js
+++
@@ -1,4 +0,0 @@
-import { createBackendServer } from './db.js';
-
-// CONNECTION BACKEND AU PORT 8800
-createBackendServer(8800);
--- my-library/client/src/store/reduc/redux-index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { sidebar } from './redux-sidebar';
-
-export const rootReducer = combineReducers({
-    sidebar: sidebar,
-})
